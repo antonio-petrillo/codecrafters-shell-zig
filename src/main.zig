180354a145c368016514ab6df8380884a27dc927
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2024-11-26 21:07:06 +0100

    refactoring done

--- src/main.zig
+++ src/main.zig
@@ -7,110 +7,151 @@ const ParseError = error{
     NoCmd,
     UnknownCmd,
     FailedParseArgs,
+    Failed,
     TooManyArgs,
     MissingArg,
 };
 
-const Command = enum {
-    exit, // built-in exit cmd
+const Builtin = enum {
+    exit,
     echo,
-    type_,
-    bad,
-    executable,
+    type_, // this may raise problems
 
-    pub fn to_str(c: Command) [:0]const u8 {
-        return switch (c) {
-            .exit => "exit",
-            .echo => "echo",
-            .type_ => "type",
-            .executable => "external executable",
-            .bad => "", // bad command: honestly I don't like it
-        };
-    }
-};
-
-const RunCommand = union(Command) {
-    exit: u8, // exit code, default to 0
-    echo: []const u8,
-    type_: Command,
-    bad: []const u8, // bad command
-    executable: [2][]const u8,
-
-    // see: https://github.com/ziglang/zig/blob/master/lib/std/zig/tokenizer.zig
-    const available_commands = std.StaticStringMap(Command).initComptime(.{
+    const builtins = std.StaticStringMap(Builtin).initComptime(.{
         .{ "exit", .exit },
         .{ "echo", .echo },
         .{ "type", .type_ },
     });
 
-    pub fn parse(allocator: Allocator, src: []const u8) ParseError!RunCommand {
+    pub fn isBuiltin(s: []const u8) bool {
+        return builtins.has(s);
+    }
+
+    pub fn getBuiltinFromStr(s: []const u8) Builtin {
+        return builtins.get(s) orelse unreachable;
+    }
+};
+
+const External = struct {
+    path: []const u8,
+    args: ?[][]const u8,
+};
+
+const Command = struct {
+    kind: union(enum) {
+        builtin: union(Builtin) {
+            exit: u8,
+            echo: []const u8,
+            type_: ?*Command,
+        },
+        extern_: External,
+        not_found: void,
+    },
+    name: []const u8,
+
+    pub fn printType(out: std.io.Writer, c: Command) !void {
+        switch (c.kind) {
+            .builtin => {
+                try out.print("{s} is a shell builtin\n", c.name);
+            },
+            .extern_ => |ext| {
+                try out.print("{s} is {s}\n", c.name, ext.path);
+            },
+            .not_found => {
+                try out.print("{s}: not found\n", c.name);
+            },
+        }
+    }
+
+    pub fn parse(allocator: Allocator, src: []const u8) ParseError!*Command {
         var token_iter = std.mem.tokenizeSequence(u8, src, " ");
 
         // in case of no token then there is no command
         const cmd_tok = token_iter.next() orelse return ParseError.NoCmd;
 
-        // if token is not in the available commands the it is an unknonw command
-        const cmd = available_commands.get(cmd_tok) orelse return ParseError.UnknownCmd;
+        if (Builtin.isBuiltin(cmd_tok)) {
+            return parseBuiltin(allocator, cmd_tok, &token_iter);
+        } else {
+            return parseExternal(allocator, cmd_tok, &token_iter);
+        }
+    }
 
-        return switch (cmd) {
+    fn parseBuiltin(allocator: Allocator, name: []const u8, token_iter: *std.mem.TokenIterator(u8, .sequence)) ParseError!*Command {
+        const cmdPtr = allocator.create(Command) catch return ParseError.Failed;
+        errdefer allocator.destroy(cmdPtr);
+
+        return switch (Builtin.getBuiltinFromStr(name)) {
             .exit => {
-                // if no more parameter are given the return `exit 0`
-                const exit_num_code = token_iter.next() orelse return RunCommand{ .exit = 0 };
-                const exit_value = std.fmt.parseInt(u8, exit_num_code, 10) catch return ParseError.FailedParseArgs;
-
+                const exit_code_str = token_iter.next() orelse {
+                    cmdPtr.* = Command{ .kind = .{ .builtin = .{ .exit = 0 } }, .name = name };
+                    return cmdPtr;
+                };
+                const exit_code = std.fmt.parseInt(u8, exit_code_str, 10) catch return ParseError.FailedParseArgs;
                 if (token_iter.peek() != null) {
                     return ParseError.TooManyArgs;
                 }
-
-                return RunCommand{ .exit = exit_value };
+                cmdPtr.* = Command{ .kind = .{ .builtin = .{ .exit = exit_code } }, .name = name };
+                return cmdPtr;
             },
             .echo => {
-                const first = token_iter.next() orelse return RunCommand{ .echo = "" };
-                const start = token_iter.index - first.len;
-                while (token_iter.peek() != null) : (_ = token_iter.next()) {}
-                const end = token_iter.index;
-                return RunCommand{
-                    .echo = src[start..end],
-                };
+                const text = token_iter.rest();
+                cmdPtr.* = Command{ .kind = .{ .builtin = .{ .echo = text } }, .name = name };
+                return cmdPtr;
             },
             .type_ => {
-                const arg = token_iter.next() orelse return ParseError.MissingArg;
-
+                const target = token_iter.next() orelse return ParseError.MissingArg;
                 if (token_iter.peek() != null) {
                     return ParseError.TooManyArgs;
                 }
 
-                if (available_commands.get(arg)) |target| {
-                    return RunCommand{ .type_ = target };
-                } else {
-                    return executablePathLookup(allocator, arg);
+                if (std.mem.eql(u8, target, "type")) {
+                    cmdPtr.* = Command{ .kind = .{ .builtin = .{
+                        .type_ = null,
+                    } }, .name = name };
+
+                    return cmdPtr;
                 }
+
+                const targetCmd = try Command.parse(allocator, target);
+                cmdPtr.* = Command{ .kind = .{ .builtin = .{ .type_ = targetCmd } }, .name = name };
+                return cmdPtr;
             },
-            .bad, .executable => unreachable,
         };
     }
-};
 
-fn executablePathLookup(allocator: Allocator, prog_name: []const u8) RunCommand {
-    const path_env = posix.getenv("PATH") orelse return RunCommand{ .bad = prog_name };
-    var path_iter = std.mem.tokenizeSequence(u8, path_env, ":");
+    fn parseExternal(allocator: Allocator, cmd: []const u8, token_iter: *std.mem.TokenIterator(u8, .sequence)) ParseError!*Command {
+        const cmdPtr = allocator.create(Command) catch return ParseError.Failed;
+        errdefer allocator.destroy(cmdPtr);
 
-    while (path_iter.peek() != null) {
-        const path = path_iter.next().?;
-        const file_path = std.fs.path.join(allocator, &.{ path, prog_name }) catch continue;
+        const path_env = posix.getenv("PATH") orelse {
+            cmdPtr.* = Command{ .kind = .not_found, .name = cmd };
+            // return Command{ .not_found = void, .name = cmd };
+            return cmdPtr;
+        };
+        var path_iter = std.mem.tokenizeSequence(u8, path_env, ":");
 
-        const file = std.fs.openFileAbsolute(file_path, .{}) catch continue;
-        defer file.close();
+        // unuesed for now
+        _ = token_iter;
 
-        const mode = file.mode() catch continue; // 0o777 -> rwxrwxrwx -> 0b111 111 111 -> 0x1 1111 1111 -> 0x100100100
-        if (mode & 0x100100100 != 0) {
-            return RunCommand{ .executable = [2][]const u8{ prog_name, file_path } };
+        while (path_iter.peek() != null) {
+            const path = path_iter.next().?;
+            const file_path = std.fs.path.join(allocator, &.{ path, cmd }) catch continue;
+
+            const file = std.fs.openFileAbsolute(file_path, .{}) catch continue;
+            defer file.close();
+
+            const mode = file.mode() catch continue; // 0o777 -> rwxrwxrwx -> 0b111 111 111 -> 0x1 1111 1111 -> 0x100100100
+            // if (mode & 0b00000000000000000000000100100100 != 0) {
+            if (mode & 0x124 != 0) {
+                cmdPtr.* = Command{ .kind = .{ .extern_ = .{ .path = file_path, .args = null } }, .name = cmd };
+                return cmdPtr;
+            }
+            allocator.free(file_path);
         }
-        allocator.free(file_path);
+        cmdPtr.* = Command{ .kind = .not_found, .name = cmd };
+        return cmdPtr;
     }
-
-    return RunCommand{ .bad = prog_name };
-}
+};
 
 fn repl(allocator: Allocator) !void {
     const stdout = std.io.getStdOut().writer();
@@ -127,44 +168,64 @@ fn repl(allocator: Allocator) !void {
 
         // The 'R' in 'REPL'
         const user_input = stdin.readUntilDelimiter(&buffer, '\n') catch return;
+        const cmd = Command.parse(arena.allocator(), user_input) catch |err| switch (err) {
+            ParseError.NoCmd => continue,
 
-        // (Handle Error) The 'E' in 'REPL'
-        const cmd = RunCommand.parse(arena.allocator(), user_input) catch |err| switch (err) {
-            error.NoCmd => continue,
-            error.UnknownCmd => {
+            ParseError.UnknownCmd => {
                 try stdout.print("{s}: command not found\n", .{user_input});
                 continue;
             },
-            error.FailedParseArgs => {
+            ParseError.FailedParseArgs => {
                 try stdout.print("{s}: bad arguments\n", .{user_input});
                 continue;
             },
-            error.TooManyArgs => {
+            ParseError.TooManyArgs => {
                 try stdout.print("Too many arguments: {s}\n", .{user_input});
                 continue;
             },
-            error.MissingArg => {
+            ParseError.MissingArg => {
                 try stdout.print("Missing argument in command: {s}\n", .{user_input});
                 continue;
             },
+            ParseError.Failed => {
+                try stdout.print("something went wrong\n", .{});
+                posix.exit(1); // something failed
+            },
         };
 
-        // The 'E' in 'REPL'
-        switch (cmd) {
-            .exit => |exit_value| {
-                posix.exit(exit_value);
+        switch (cmd.kind) {
+            .builtin => |b| {
+                switch (b) {
+                    .exit => |exit_code| {
+                        posix.exit(exit_code);
+                    },
+                    .echo => |str| {
+                        try stdout.print("{s}\n", .{str});
+                    },
+                    .type_ => |maybe_arg| {
+                        const arg = maybe_arg orelse {
+                            try stdout.print("type is a shell builtin\n", .{});
+                            continue;
+                        };
+                        switch (arg.kind) {
+                            .builtin => {
+                                try stdout.print("{s} is a shell builtin\n", .{arg.name});
+                            },
+                            .extern_ => |exe| {
+                                try stdout.print("{s} is {s}\n", .{ arg.name, exe.path });
+                            },
+                            .not_found => {
+                                try stdout.print("{s}: not found\n", .{arg.name});
+                            },
+                        }
+                    },
+                }
             },
-            .echo => |str| {
-                try stdout.print("{s}\n", .{str});
+            .extern_ => {
+                continue; // nothing for now
             },
-            .type_ => |c| {
-                try stdout.print("{s} is a shell builtin\n", .{c.to_str()});
-            },
-            .executable => |arr| {
-                try stdout.print("{s} is {s}\n", .{ arr[0], arr[1] });
-            },
-            .bad => |s| {
-                try stdout.print("{s}: not found\n", .{s});
+            .not_found => {
+                try stdout.print("{s}: not found\n", .{cmd.name});
             },
         }
     }
